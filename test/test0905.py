ddbc7a725eaacfb5a168ae902e54fb0cc552950d
test1 <inner_ert@qq.com>
2017-09-14 17:47:34 +0800

    测试

---
+++ test/test0905.py
@@ -0,0 +1,48 @@
+#coding:utf-8
+
+name = 'My name is Mike'
+print name[11]
+
+
+#100-999三位数的水仙花数
+# L = []
+# for i in range(100,999):
+#         a = i/100
+#         b = (i-100*a)/10
+#         c = i%10
+#         d = a**3 + b**3 +c**3
+#         if d == i:
+#             L.append(i)
+# print  '100-999三位数的水仙花数有%s' %L
+
+
+
+
+
+
+
+#99乘法表
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print '%d*%d=%-2d'% (i , j , i*j) , 
+#     print 
+
+
+
+
+
+
+
+# L = []
+# for i in range(1,1000):
+#      sum = 0
+#      for k in range(1,i+1):
+#          if i % k ==0 and k != i:  #计算因子
+#             sum = sum +k  #计算真因子之和
+#             if sum == i:
+#                  L.append(i)
+# print '1000以内的完全数有：%s' %L 
+
+
+
+
