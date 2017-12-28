05d3f251a28457525e53c79e358b6d3464dbb01f
test1 <inner_ert@qq.com>
2017-12-28 14:18:10 +0800

    Python基础练习题

--- practice/practice_03.py
+++ practice/practice_03.py
@@ -3,5 +3,9 @@
 # 再加上168又是一个完全平方数，请问该数是多少？
 import math
 
-
-
+for m in range(1,168):
+    for n in range(1,m):
+        if ((m+n)*(m-n))==168:
+            x = n**2-100
+            if x >2 and x< 168:
+                print("这个数是%s"%x)
